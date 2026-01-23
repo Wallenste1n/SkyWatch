d661e6086e62e2b055536d619f1aebb71e9734c2
Wallenstein <vlad.bondart@gmail.com>
2026-01-23 20:17:07 +0100

    Privacy View deleted

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,12 +15,6 @@ public class HomeController : Controller
         return View(weatherController.GetWeatherData());
     }
 
-    public IActionResult Privacy()
-    {
-        return View();
-        
-    }
-
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
