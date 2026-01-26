f525c27ed7c89cc0a3af13fe8141a00deba08c4d
Wallenstein <vlad.bondart@gmail.com>
2026-01-26 13:49:18 +0100

    WeatherService is implements IWeatherService interface It contains logic from WeatherController (now it's deleted file) with rewrited style for better readability and to lower dependecies, how it was in WeatherController

---
+++ Services/WeatherService.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using SkyWatch.Interfaces;
+using SkyWatch.Models;
+
+namespace SkyWatch.Services;
+
+//Implements IWeatherService
+public class WeatherService : IWeatherService
+{
+    private readonly IConfiguration _config;
+    private readonly HttpClient _httpClient;
+
+    //For getting config and httpClient classes works
+    //+ to lower dependencies in Controllers
+    public WeatherService(IConfiguration config, HttpClient httpClient)
+    {
+        _config = config;
+        _httpClient = httpClient;
+    }
+
+    //Getting information from API response and send its info to WeatherModel in async
+    public async Task<WeatherModel> GetWeatherAsync(string city)
+    {
+        //apiKey is getting Key for WeatherAPI from the JSON file
+        string? apiKey = _config["Key"];
+        string units = "metric";
+        string lang = "en";
+        string url = $"https://api.openweathermap.org/data/2.5/weather" +
+                     $"?q={city}&appid={apiKey}&units={units}&lang={lang}";
+
+        var response = await _httpClient.GetStringAsync(url);
+        return JsonConvert.DeserializeObject<WeatherModel>(response);
+    }
+}
