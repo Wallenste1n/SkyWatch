a1e4758aa102ce073975cbfe5358be363055fa65
Wallenstein <vlad.bondart@gmail.com>
2026-01-26 13:37:54 +0100

    Added interfaces architecture logic for better program architecture

---
+++ Interfaces/IWeatherService.cs
@@ -0,0 +1,10 @@
+﻿using SkyWatch.Models;
+
+namespace SkyWatch.Interfaces;
+
+//interface for implementing functions in WeatherService
+public interface IWeatherService
+{
+    //for getting api and it's data in async
+    Task<WeatherModel> GetWeatherAsync(string city);
+}
