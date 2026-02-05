582026d21ae5a8956803540b90e901c14a8899b6
Wallenstein <vlad.bondart@gmail.com>
2026-02-05 14:47:33 +0100

    Model for getting inputs from user through UI Gets name of the city, chosen units, language of the site and switches units from metric

---
+++ Models/WeatherViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace SkyWatch.Models;
+
+public class WeatherViewModel
+{
+    //UI state
+    public string cityName { get; set; } = "London";
+    public string units { get; set; } = "metric";
+    public string lang { get; set; } = "en";
+    
+    public WeatherModel? Weather { get; set; }
+
+    //switches API metrics to it units
+    public string TemperatureUnit =>
+        units switch
+        {
+            "metric" => "°C",
+            "imperial" => "°F",
+            "standard" => "K",
+            _ => ""
+        };
+}
