8274c3251f7b8108ce5f826a3c7352e003029620
Wallenstein <vlad.bondart@gmail.com>
2026-02-05 14:43:32 +0100

    Added 2 functions First: gets units and form and submit it immediately Second: is looking for user location throught IP, and writes his location in cookie, if cookie is already exist, it skips this function

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,33 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.addEventListener("DOMContentLoaded", () =>{
+    //Gets units and form from document to submit it after pressing each select or submit 
+    const units = document.getElementById("unitsSelect");
+    const form = document.getElementById("weatherForm");
+    if(units && form){
+        units.addEventListener("change", () => form.submit());
+    }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    //If city is in cookie - doing nothing
+    if(document.cookie.includes("weather_city")) return;
+    
+    //Looking for location through user's IP
+    fetch("https://ipapi.co/json/")
+        .then(r => r.json())
+        .then(data => {
+            if(!data.city) return;
+            
+            //writes cookie
+            document.cookie = `weather_city=${data.city}; path=/; max-age=2592000`;
+            
+            //sends form immediately
+            const form = document.getElementById("weatherForm");
+            if(form){
+                form.querySelector("input[name='cityName']").value = data.city;
+                form.submit();
+            }
+        })
+})
