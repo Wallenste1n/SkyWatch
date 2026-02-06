d6745053833e0bb9edb45dc798ac36e297454668
Wallenstein <vlad.bondart@gmail.com>
2026-02-06 23:57:19 +0100

    Created helper for getting geographical directions by degrees

---
+++ Helpers/WindDirectionHelper.cs
@@ -0,0 +1,31 @@
+﻿namespace SkyWatch.Helper;
+
+public class WindDirectionHelper
+{
+    //Gets names of geographical directions by it's degrees
+    public static string GetDirection(double degrees)
+    {
+        degrees = (degrees + 360) % 360;
+
+        switch (degrees)
+        {
+            case >= 337.5:
+            case < 22.5:
+                return "Wind_North";
+            case >= 22.5 and < 67.5:
+                return "Wind_NorthEast";
+            case >= 67.5 and < 112.5:
+                return "Wind_East";
+            case >= 112.5 and < 157.5:
+                return "Wind_SouthEast";
+            case >= 157.5 and < 202.5:
+                return "Wind_South";
+            case >= 202.5 and < 247.5:
+                return "Wind_SouthWest";
+            case >= 247.5 and < 295.5:
+                return "Wind_West";
+            default:
+                return "Wind_NorthWest";
+        }
+    }
+}
