ff19885a11c0f639e9d1adab09c5016b44f319cf
Wallenstein <vlad.bondart@gmail.com>
2026-02-06 23:37:56 +0100

    Added enum for handling different types of errors

---
+++ Models/WeatherErrorType.cs
@@ -0,0 +1,13 @@
+﻿namespace SkyWatch.Models;
+
+//Enum of error types to handle
+public enum WeatherErrorType
+{
+    None,
+    CityEmpty,
+    CityNotFound,
+    ApiUnavailable,
+    InvalidApiKey,
+    TooManyRequests,
+    Unknown
+}
