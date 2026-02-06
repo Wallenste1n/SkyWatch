4cfb64202bafdcc51a04ba015ba96f4fbae018dc
Wallenstein <vlad.bondart@gmail.com>
2026-02-06 23:38:35 +0100

    Class for error type handling

---
+++ Models/WeatherServiceResult.cs
@@ -0,0 +1,8 @@
+﻿namespace SkyWatch.Models;
+
+//Model mostly to connect WeatherModel and error type handling
+public class WeatherServiceResult
+{
+    public WeatherModel? Weather { get; set; }
+    public WeatherErrorType ErrorType { get; set; } = WeatherErrorType.None;
+}
