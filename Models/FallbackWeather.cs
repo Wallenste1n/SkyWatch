816a7582116d0feb88e16e782cd5d648cbace463
Wallenstein <vlad.bondart@gmail.com>
2026-02-06 22:20:05 +0100

    Added new class for containg data for fallback, if something happened on the site

---
+++ Models/FallbackWeather.cs
@@ -0,0 +1,9 @@
+﻿namespace SkyWatch.Models;
+
+//Contains info for fallback
+public class FallbackWeather
+{
+    public string City { get; set; }
+    public string Units { get; set; }
+    public WeatherModel? Weather { get; set; }
+}
