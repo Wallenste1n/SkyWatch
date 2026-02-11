1773621ded4564b503a676f6593e55109cb70e38
Wallenstein <vlad.bondart@gmail.com>
2026-02-12 00:59:20 +0100

    New model to get info of the city (name, lat, lon, country, state, (if available))

---
+++ Models/WeatherGeoCoderModel.cs
@@ -0,0 +1,20 @@
+﻿namespace SkyWatch.Models;
+
+//Geo Coder API handler
+public class WeatherGeoCoderModel
+{
+    //name of the city
+    public string name { get; set; }
+    
+    //latitude of the city location
+    public double lat { get; set; }
+    
+    //longitude of the city location
+    public double lon { get; set; }
+    
+    //country of the city
+    public string country { get; set; }
+    
+    //state of the city (if available)
+    public string state { get; set; }
+}
