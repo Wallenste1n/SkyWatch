af0d5be074d078676dd711538d399f1722258f97
Wallenstein <vlad.bondart@gmail.com>
2026-02-12 01:04:23 +0100

    Storage Geo Coder API response info

---
+++ Models/CitySearchResult.cs
@@ -0,0 +1,16 @@
+﻿namespace SkyWatch.Models;
+
+//To contain Result from API
+public class CitySearchResult
+{
+    public string Name { get; set; }
+    public string Country { get; set; }
+    public string State { get; set; }
+    public double Lat { get; set; }
+    public double Lon { get; set; }
+
+    public string DisplayName =>
+        string.IsNullOrEmpty(State)
+            ? $"{Name}, {Country}"
+            : $"{Name}, {Country}, {State}";
+}
