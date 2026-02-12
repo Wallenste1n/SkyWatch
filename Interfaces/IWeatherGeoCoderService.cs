ef13d44c8d734f591a022fc5d54f618b73a8b03a
Wallenstein <vlad.bondart@gmail.com>
2026-02-12 01:03:43 +0100

    New interface for WeatherGeoCoder service with 2 func 1. Get's coordinates (lat, lon) by city name 2. Search for cities from users dynamic info to get suggestions for him

---
+++ Interfaces/IWeatherGeoCoderService.cs
@@ -0,0 +1,9 @@
+﻿using SkyWatch.Models;
+
+namespace SkyWatch.Interfaces;
+
+public interface IWeatherGeoCoderService
+{
+    Task<WeatherServiceResult> GetCityCoordinatesAsync(string cityName);
+    Task<List<CitySearchResult>> SearchCitiesAsync(string query);
+}
