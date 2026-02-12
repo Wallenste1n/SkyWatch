f854274c7f33e02a0caaf08435321b28ef3bea9e
Wallenstein <vlad.bondart@gmail.com>
2026-02-12 01:00:33 +0100

    Added WeatherGeoCoderService and it's interface to do DI

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 //Services for Localization and for working with WeatherService
 builder.Services.AddHttpClient<IWeatherService ,WeatherService>();
+builder.Services.AddHttpClient<IWeatherGeoCoderService, WeatherGeoCoderService>();
 builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
 builder.Services.AddControllersWithViews().AddViewLocalization().AddDataAnnotationsLocalization();
 builder.Services.AddHttpContextAccessor();
