3be5eceb1b38dc3f6465603564d5ce68ad8f37b0
Wallenstein <vlad.bondart@gmail.com>
2026-02-15 21:26:47 +0100

    New Service for forecast API to get info from it

---
+++ Services/WeatherHourlyForecastService.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using SkyWatch.Interfaces;
+using SkyWatch.Models;
+using SkyWatch.Models.ApiModels;
+using SkyWatch.Models.ServiceResultsModels;
+
+namespace SkyWatch.Services;
+
+public class WeatherHourlyForecastService : IWeatherHourlyForecastService
+{
+    private readonly IConfiguration _config;
+    private readonly HttpClient _httpClient;
+
+    public WeatherHourlyForecastService(IConfiguration config, HttpClient httpClient)
+    {
+        _config = config;
+        _httpClient = httpClient;
+    }
+    
+    public async Task<WeatherServiceResult> GetHourlyForecast(double lat, double lon, string units, string lang)
+    {
+        string? apiKey = _config["Key"];
+        
+        if (string.IsNullOrWhiteSpace(apiKey))
+            return new WeatherServiceResult { ErrorType = WeatherErrorType.InvalidApiKey };
+        
+        string cnt = "24";
+        string url = $"https://pro.openweathermap.org/data/2.5/forecast/hourly?" +
+                     $"lat={lat}&lon={lon}&appid={apiKey}&cnt={cnt}&units={units}&lang={lang}";
+
+        var response = await _httpClient.GetAsync(url);
+        string json = await response.Content.ReadAsStringAsync();
+        var forecast = JsonConvert.DeserializeObject<WeatherHourlyForecastModel>(json);
+
+        return new WeatherServiceResult { HourlyForecast = forecast, ErrorType = WeatherErrorType.None };
+    }
+}
