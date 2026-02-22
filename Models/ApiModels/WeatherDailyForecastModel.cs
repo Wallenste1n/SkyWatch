f6b71a3248b558692f46f84e21fd071c07824930
Wallenstein <vlad.bondart@gmail.com>
2026-02-22 02:24:10 +0100

    New model for dailt forecast api request

---
+++ Models/ApiModels/WeatherDailyForecastModel.cs
@@ -0,0 +1,21 @@
+﻿namespace SkyWatch.Models.ApiModels;
+
+//For daily forecast Api requests
+public class WeatherDailyForecastModel
+{
+    public List<ListOfData>? list { get; set; }
+}
+
+public class ListOfData
+{
+    public long dt { get; set; }
+    public Temperature temp { get; set; }
+    public List<Weather>? weather { get; set; }
+}
+
+public class Temperature
+{
+    public double day { get; set; }
+    public double min { get; set; }
+    public double max { get; set; }
+}
