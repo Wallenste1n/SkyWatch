85999d79504c4035ba2edc3663772251c35a2638
Wallenstein <vlad.bondart@gmail.com>
2026-02-22 02:35:51 +0100

    Added EN localization for variables - Today, Tomorrow

--- Resources/Views/Home/Index.en.Designer.cs
+++ Resources/Views/Home/Index.en.Designer.cs
@@ -240,6 +240,24 @@ namespace SkyWatch.Resources.Views.Home {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Today.
+        /// </summary>
+        internal static string Today {
+            get {
+                return ResourceManager.GetString("Today", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Tomorrow.
+        /// </summary>
+        internal static string Tomorrow {
+            get {
+                return ResourceManager.GetString("Tomorrow", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Too many requests. Please try later..
         /// </summary>
         internal static string TooManyRequests {
