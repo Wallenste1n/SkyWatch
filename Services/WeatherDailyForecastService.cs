0e0c6aef05461fd6260b4698088e31440689fe04
Wallenstein <vlad.bondart@gmail.com>
2026-02-22 02:24:35 +0100

    New service for daily forecast Api

---
+++ Services/WeatherDailyForecastService.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using SkyWatch.Interfaces;
+using SkyWatch.Models;
+using SkyWatch.Models.ApiModels;
+using SkyWatch.Models.ServiceResultsModels;
+
+namespace SkyWatch.Services;
+
+public class WeatherDailyForecastService : IWeatherDailyForecastService
+{
+    private readonly IConfiguration _config;
+    private readonly HttpClient _httpClient;
+
+    public WeatherDailyForecastService(IConfiguration config, HttpClient httpClient)
+    {
+        _config = config;
+        _httpClient = httpClient;
+    }
+    
+    //Gets info from daily forecast API
+    public async Task<WeatherServiceResult> GetDailyForecastAsync(double lat, double lon, string units, string lang)
+    {
+        string? apiKey = _config["Key"];
+
+        if (string.IsNullOrWhiteSpace(apiKey))
+            return new WeatherServiceResult { ErrorType = WeatherErrorType.InvalidApiKey };
+        
+        string cnt = "7";
+        string url = $"https://api.openweathermap.org/data/2.5/forecast/daily?" +
+                     $"lat={lat}&lon={lon}&cnt={cnt}&appid={apiKey}&lang={lang}&units={units}";
+
+        var response = await _httpClient.GetAsync(url);
+        var json = await response.Content.ReadAsStringAsync();
+        var dailyForecast = JsonConvert.DeserializeObject<WeatherDailyForecastModel>(json);
+
+        return new WeatherServiceResult { DailyForecast = dailyForecast };
+    }
+}
