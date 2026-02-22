6e24b16d27692e6e60b2ff46b38dc656d5eed7e5
Wallenstein <vlad.bondart@gmail.com>
2026-02-22 02:23:36 +0100

    New interface for the dailt forecast

---
+++ Interfaces/IWeatherDailyForecastService.cs
@@ -0,0 +1,8 @@
+﻿using SkyWatch.Models.ServiceResultsModels;
+
+namespace SkyWatch.Interfaces;
+
+public interface IWeatherDailyForecastService
+{
+    Task<WeatherServiceResult> GetDailyForecastAsync(double lat, double lon, string units, string lang);
+}
