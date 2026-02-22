350f02d113609bdf7edd60611a2aa999c752d16d
Wallenstein <vlad.bondart@gmail.com>
2026-02-22 01:55:46 +0100

    New file for unit selector logic Function added: Shows active button of the chosen temp unit

---
+++ wwwroot/js/unitSelector.js
@@ -0,0 +1,15 @@
+﻿const unitButtons = document.querySelectorAll(".unit-btn");
+const unitsInput = document.getElementById("unitsInput");
+const form = document.getElementById('weatherForm');
+
+// Shows active state of the button of the chosen temp unit
+unitButtons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        unitButtons.forEach(b => b.classList.remove("active"));
+        btn.classList.add("active");
+        
+        unitsInput.value = btn.dataset.unit;
+        
+        form.submit();
+    });
+});
