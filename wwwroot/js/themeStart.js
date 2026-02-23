58094b5e07e09c41a412050570f636bd03fee3dd
Wallenstein <vlad.bondart@gmail.com>
2026-02-23 19:39:59 +0100

    New file for the more smooth appearance of the chosen theme (without this, it flickers)

---
+++ wwwroot/js/themeStart.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    try {
+        const savedTheme = localStorage.getItem("theme");
+
+        if (savedTheme === "dark") {
+            document.documentElement.setAttribute("data-theme", "dark");
+        }
+        else if (savedTheme === "light") {
+            document.documentElement.setAttribute("data-theme", "light");
+        }
+        else {
+            // Auto-detect from OS
+            const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+            document.documentElement.setAttribute(
+                "data-theme",
+                prefersDark ? "dark" : "light"
+            );
+        }
+    } catch (e) {}
+})();
